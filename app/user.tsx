507f352bab08a6252760cf9e1fad8a3f321f969f
Simoncar <simoncar@gmail.com>
2023-06-20 15:44:03 +0200

    proilfe screen

--- app/user.tsx
+++ app/user.tsx
@@ -1,16 +1,20 @@
 import { Stack, useLocalSearchParams } from "expo-router";
 import React, { useContext, useState } from "react";
-import { Button, SafeAreaView, StyleSheet } from "react-native";
+import { Button, SafeAreaView, StyleSheet, useColorScheme } from "react-native";
 import { Text, View } from "../components/Themed";
 import ProjectContext from "../lib/context";
 import { useRouter } from "expo-router";
 import { Image } from "expo-image";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Colors from "../constants/Colors";
 
 export default function editPost() {
 	const { sharedData } = useContext(ProjectContext);
 	const { project, key, image, caption } = useLocalSearchParams();
 	const [text, onChangeText] = useState(caption);
 	const router = useRouter();
+	const colorScheme = useColorScheme();
 
 	const save = () => {
 		router.push({
@@ -23,7 +27,7 @@ export default function editPost() {
 
 	return (
 		<SafeAreaView>
-			<View style={styles.container}>
+			<View style={styles.avatarContainer}>
 				<View style={styles.avatar}>
 					<Image
 						style={styles.avatarFace}
@@ -35,6 +39,34 @@ export default function editPost() {
 					<Text style={styles.name}>Jacob Graham</Text>
 				</View>
 			</View>
+
+			<View style={styles.outerView}>
+				<View style={styles.leftContent}>
+					<FontAwesome5 name="address-card" size={25} color={Colors[colorScheme ?? "light"].text} />
+					<Text style={styles.settingName}>Contact Details</Text>
+				</View>
+				<View style={styles.rightChevron}>
+					<FontAwesome5 name="chevron-right" size={20} color={Colors[colorScheme ?? "light"].text} />
+				</View>
+			</View>
+
+			<View style={styles.outerView}>
+				<View style={styles.leftContent}>
+					<FontAwesome5 name="trash-alt" size={25} color={Colors[colorScheme ?? "light"].text} />
+					<Text style={styles.settingName}>Delete Account</Text>
+				</View>
+				<View style={styles.rightChevron}>
+					<FontAwesome5 name="chevron-right" size={20} color={Colors[colorScheme ?? "light"].text} />
+				</View>
+			</View>
+
+			<View style={styles.outerView}>
+				<View style={styles.leftContent}>
+					<MaterialIcons name="logout" size={25} color={Colors[colorScheme ?? "light"].text} />
+					<Text style={styles.settingName}>Log Out</Text>
+				</View>
+				<View style={styles.rightChevron}></View>
+			</View>
 		</SafeAreaView>
 	);
 }
@@ -42,15 +74,42 @@ export default function editPost() {
 const styles = StyleSheet.create({
 	avatarFace: { width: 100, height: 100, borderRadius: 100 / 2 },
 	avatar: {},
-	container: {
+	avatarContainer: {
 		alignItems: "center",
 		justifyContent: "center",
 		paddingTop: 50
 	},
+	outerView: {
+		borderBottomColor: "#CED0CE",
+		borderBottomWidth: StyleSheet.hairlineWidth,
+		flexDirection: "row",
+		paddingVertical: 8,
+		alignItems: "center",
+		padding: 8,
+		height: 80
+	},
+	nameContainer: {
+		paddingBottom: 50,
+		paddingTop: 20
+	},
+	leftContent: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "flex-start",
+		alignItems: "center",
+		paddingHorizontal: 8
+	},
 	name: {
-		justifyContent: "center",
-		paddingTop: 20,
 		fontSize: 20,
-		fontWeight: "bold"
+		fontWeight: "bold",
+		paddingLeft: 20
+	},
+
+	settingName: {
+		fontSize: 20,
+		paddingLeft: 20
+	},
+	rightChevron: {
+		marginHorizontal: 8
 	}
 });
