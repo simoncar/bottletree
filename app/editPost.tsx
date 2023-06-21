16e3c877a2ebed8d5b21d810910fed2799a35d92
Simoncar <simoncar@gmail.com>
2023-06-21 16:08:25 +0200

    Post Layout

--- app/editPost.tsx
+++ app/editPost.tsx
@@ -1,21 +1,20 @@
 import React, { useContext, useState, useEffect } from "react";
-import { StyleSheet, SafeAreaView, Button, Alert } from "react-native";
+import Entypo from "@expo/vector-icons/Entypo";
+import { StyleSheet, SafeAreaView, Button, Alert, useColorScheme } from "react-native";
 import { Image } from "expo-image";
 import { useRouter, useLocalSearchParams, Stack } from "expo-router";
 import { updatePost, deletePost } from "../lib/APIpost";
 import ProjectContext from "../lib/context";
-import { TextInput, View } from "../components/Themed";
+import { TextInput, View, Text } from "../components/Themed";
+import Colors from "../constants/Colors";
 
 export default function editPost() {
 	const { sharedData } = useContext(ProjectContext);
 	const { project, key, image, caption } = useLocalSearchParams();
 	const [text, onChangeText] = useState(caption);
+	const colorScheme = useColorScheme();
 
 	const router = useRouter();
-
-	useEffect(() => {
-		<Stack.Screen options={{ title: "Overview" }} />;
-	}, []);
 
 	const saveDone = () => {
 		router.push({
@@ -81,8 +80,15 @@ export default function editPost() {
 					headerRight: () => <Button title="Done" onPress={() => save()} />
 				}}
 			/>
-
 			{image && <Image source={image} style={styles.storyPhoto} />}
+			<View style={styles.outerView}>
+				<View style={styles.leftContent}>
+					<Text style={styles.settingName}>Delete Account</Text>
+				</View>
+				<View style={styles.rightChevron}>
+					<Entypo name="dots-three-horizontal" size={25} color={Colors[colorScheme ?? "light"].text} />
+				</View>
+			</View>
 			<TextInput style={styles.input} onChangeText={(text) => onChangeText(text)} placeholder={"Write a caption..."} value={text} autoFocus multiline />
 			<View style={styles.actions}>
 				<View style={styles.button}>
@@ -109,6 +115,26 @@ const styles = StyleSheet.create({
 		paddingLeft: 20,
 		width: "98%",
 		fontSize: 20
+	},
+	leftContent: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "flex-start",
+		alignItems: "center",
+		paddingHorizontal: 8
+	},
+	rightChevron: {
+		marginHorizontal: 8
+	},
+
+	outerView: {
+		borderBottomColor: "#CED0CE",
+		borderBottomWidth: StyleSheet.hairlineWidth,
+		flexDirection: "row",
+		paddingVertical: 8,
+		alignItems: "center",
+		padding: 8,
+		height: 80
 	},
 	button: {
 		marginTop: 50,
