875cbf8052ac20709eee23d3831244c43c326b33
Simoncar <simoncar@gmail.com>
2023-07-26 14:13:29 +0200

    actions

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -9,6 +9,20 @@ import {
 } from "firebase/auth/react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+
+
+
+export const firebaseErrors: Record<string, string> = {
+  "Firebase: Error (auth/email-already-in-use).":
+    "This email is already is use.",
+  "Firebase: Error (auth/popup-closed-by-user).":
+    "Process got canceled by user.",
+  "Firebase: Error (auth/user-not-found).": "User not found.",
+  "Firebase: Error (auth/wrong-password).": "Wrong email or password.",
+  "Firebase: Error (auth/network-request-failed).":
+    "Failed to send the request. Check your connection.",
+};
+
 const firebaseConfig = {
     apiKey: `${fbConfig.apiKey}`,
     authDomain: `${fbConfig.authDomain}`,
