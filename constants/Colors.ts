079f1682a2f1535bcd3de3fc668d546cd3cabe88
Simoncar <simoncar@gmail.com>
2023-07-26 18:45:24 +0200

    dark mode dots

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,31 +2,35 @@ const tintColorLight = "#2f95dc";
 const tintColorDark = "#fff";
 
 export default {
-    light: {
-        text: "#000",
-        orangeButtonIcon: "#fff",
-        textPlaceholder: "#ccc",
-        background: "#fff",
-        tint: tintColorLight,
-        calendarPanel: "#e9f0fd",
-        calendarTitle: "#3265cb",
-        calendarDayTextColor: "#2d4150",
-        textDisabledColor: "#2d4150",
-        projectPanel: "#e9f0fd",
-    },
-    dark: {
-        text: "#fff",
-        orangeButtonIcon: "#000",
-        textPlaceholder: "#ccc",
-        background: "#000",
-        tint: tintColorDark,
-        calendarPanel: "#2f95dc",
-        calendarTitle: "#fff",
-        calendarDayTextColor: "#fff",
-        textDisabledColor: "#fff",
-        projectPanel: "#232323",
-    },
+  light: {
+    text: "#000",
+    orangeButtonIcon: "#fff",
+    textPlaceholder: "#ccc",
+    background: "#fff",
+    tint: tintColorLight,
+    calendarPanel: "#e9f0fd",
+    calendarTitle: "#3265cb",
+    calendarDayTextColor: "#2d4150",
+    textDisabledColor: "#2d4150",
+    projectPanel: "#e9f0fd",
+    dots: "#232323",
+    dotsActive: "#3265cb",
+  },
+  dark: {
+    text: "#fff",
+    orangeButtonIcon: "#000",
+    textPlaceholder: "#ccc",
+    background: "#000",
+    tint: tintColorDark,
+    calendarPanel: "#2f95dc",
+    calendarTitle: "#fff",
+    calendarDayTextColor: "#fff",
+    textDisabledColor: "#fff",
+    projectPanel: "#232323",
+    dots: "#e9f0fd",
+    dotsActive: "#3265cb",
+  },
 };
 
 export const blurhash =
-    "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
