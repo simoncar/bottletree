adf29b83b1dc3a7b07f1c9bd2a8b60709fb20ea7
Simoncar <simoncar@gmail.com>
2023-08-09 11:03:40 +0200

    remove emulator

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -36,8 +36,8 @@ const auth = initializeAuth(app, {
 const db = getFirestore();
 const storage = getStorage(app);
 
-connectFirestoreEmulator(db, "localhost", 8080);
-connectStorageEmulator(storage, "localhost", 9199);
-connectAuthEmulator(auth, "http://localhost:9099/auth");
+// connectFirestoreEmulator(db, "localhost", 8080);
+// connectStorageEmulator(storage, "localhost", 9199);
+// connectAuthEmulator(auth, "http://localhost:9099/auth");
 
 export { db, storage, auth };
