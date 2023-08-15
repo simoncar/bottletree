b267b14d2e822dd370e4382e3bd3d4320a555fc5
Simoncar <simoncar@gmail.com>
2023-08-15 15:02:32 +0200

    post comments

--- lib/APIpost.ts
+++ lib/APIpost.ts
@@ -13,21 +13,26 @@ import {
 } from "firebase/firestore";
 import { IPost, IComment } from "./types";
 
-export function addPost(post: IPost, callback: saveDone) {
-  try {
-    const docRef = addDoc(collection(db, "projects", post.projectId, "posts"), {
+export async function addPost(post: IPost, callback: saveDone) {
+  const docRef = await addDoc(
+    collection(db, "projects", post.projectId, "posts"),
+    {
       author: "Ada",
       avatar:
         "https://firebasestorage.googleapis.com/v0/b/builder-403d5.appspot.com/o/demo%2Fprofile%2FScreenshot%202023-05-30%20at%202.47.44%20PM.png?alt=media&token=30888878-15e6-4395-b3d4-53ae17758e33&_gl=1*pyfxsn*_ga*MTc3ODA4OTA3Ni4xNjg0MTQ0OTY0*_ga_CW55HF8NVT*MTY4NTQ1MDg3Ni44LjEuMTY4NTQ1MDkxMS4wLjAuMA..",
       images: post.images,
       ratio: Number(post.ratio),
       timestamp: Timestamp.now(),
-    }).then((docRef) => {
-      callback(docRef.id);
-    });
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
+    },
+  );
+
+  const messageRef = await addDoc(collection(db, "messages"), {
+    title: "New Post Added",
+    body: "Some Post Name TODO",
+    timestamp: Timestamp.now(),
+  });
+
+  callback(docRef.id);
 
   return;
 }
@@ -104,26 +109,28 @@ export async function getComments(projectId: string, postId: string) {
   return comments;
 }
 
-export function addComment(
+export async function addComment(
   project: string,
   post: string,
   comment: IComment,
   callback: { (comment: IComment): void; (arg0: string): void },
 ) {
-  try {
-    addDoc(
-      collection(db, "projects", project, "posts", post, "comments"),
-      comment,
-    ).then((docRef) => {
-      const newData = { key: docRef.id };
+  const docRef = await addDoc(
+    collection(db, "projects", project, "posts", post, "comments"),
+    comment,
+  );
 
-      console.log("callback: ", { ...comment, ...newData });
+  const newData = { key: docRef.id };
 
-      callback({ ...comment, ...newData });
-    });
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
+  const messageRef = await addDoc(collection(db, "messages"), {
+    title: "New Comment Added",
+    body: "Some Comment TODO",
+    timestamp: Timestamp.now(),
+  });
+
+  console.log("callback: ", { ...comment, ...newData });
+
+  callback({ ...comment, ...newData });
 
   return;
 }
