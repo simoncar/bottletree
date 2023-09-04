a5b7d9d69c57728fde3a260b52d9c5bdee8ae487
Simoncar <simoncar@gmail.com>
2023-09-04 12:29:49 +0200

    metro

--- metro.config.js
+++ metro.config.js
@@ -3,11 +3,6 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.assetExts.push("cjs");
-defaultConfig.resolver.assetExts.push("json");
-defaultConfig.resolver.assetExts.push("jsx");
-defaultConfig.resolver.assetExts.push("js");
-defaultConfig.resolver.assetExts.push("ts");
-defaultConfig.resolver.assetExts.push("tsx");
 
 module.exports = defaultConfig;
 
