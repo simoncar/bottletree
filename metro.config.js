b27729f646df47cd36d28b94423e13c73b214f62
Simoncar <simoncar@gmail.com>
2023-09-05 10:56:51 +0200

    trying to fix mjs issue

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExts.push("cjs");
 defaultConfig.resolver.assetExts.push("mjs");
 
 module.exports = defaultConfig;
