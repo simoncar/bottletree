f981586a158f94cec722c4ab6b950d085865ccf2
Simoncar <simoncar@gmail.com>
2023-12-13 14:30:34 +0000

    Add cameraRef and takePhoto function Update camera buttons styling

--- app/camera.tsx
+++ app/camera.tsx
@@ -1,5 +1,5 @@
 import { Camera, CameraType } from "expo-camera";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {
   Button,
   StyleSheet,
@@ -15,6 +15,7 @@ export default function App() {
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const colorScheme = useColorScheme();
+  const cameraRef = useRef();
 
   if (!permission) {
     // Camera permissions are still loading
@@ -42,28 +43,53 @@ export default function App() {
     );
   }
 
+  const takePhoto = async () => {
+    if (!permission) return;
+    if (cameraRef.current) {
+      const options = { quality: 0.7 };
+      const photo = await cameraRef.current.takePictureAsync(options);
+      console.log(photo);
+      setPreviewVisible(true);
+      setCapturedImage(photo);
+    }
+
+    const photo = await Camera.takePictureAsync();
+    console.log(photo);
+    setPreviewVisible(true);
+    setCapturedImage(photo);
+  };
+
+  function flipCameraButton() {
+    return (
+      <TouchableOpacity style={styles.flipCamera} onPress={takePhoto}>
+        <Ionicons
+          name="ios-camera-reverse-outline"
+          size={45}
+          color={Colors[colorScheme ?? "light"].textPlaceholder}
+        />
+      </TouchableOpacity>
+    );
+  }
+
+  function takePhotoButton() {
+    return (
+      <TouchableOpacity onPress={toggleCameraType}>
+        <View style={styles.circleOuter}>
+          <View style={styles.circleMiddle}>
+            <View style={styles.circleInner}></View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
+      <Camera ref={cameraRef.current} style={styles.camera} type={type}>
         <View style={styles.buttonRow}>
-          <TouchableOpacity onPress={toggleCameraType}>
-            <View style={styles.circleOuter}>
-              <View style={styles.circleMiddle}>
-                <View style={styles.circleInner}></View>
-              </View>
-            </View>
-          </TouchableOpacity>
-          <View style={styles.toggleContainer}>
-            <TouchableOpacity
-              style={styles.toggleContainerButton}
-              onPress={toggleCameraType}>
-              <Ionicons
-                name="ios-camera-reverse-outline"
-                size={45}
-                color={Colors[colorScheme ?? "light"].textPlaceholder}
-              />
-            </TouchableOpacity>
-          </View>
+          <View style={styles.a}></View>
+          <View style={styles.a}>{takePhotoButton()}</View>
+          <View style={styles.a}>{flipCameraButton()}</View>
         </View>
       </Camera>
     </View>
@@ -71,11 +97,16 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  buttonRow: {
-    bottom: 0,
+  a: {
     flex: 1,
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "center",
+    paddingBottom: 40,
+  },
+  buttonRow: {
+    bottom: 0,
+    flexDirection: "row",
+    justifyContent: "center",
     position: "absolute",
   },
   camera: {
@@ -107,15 +138,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
   },
+  flipCamera: {
+    alignItems: "center",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
   permission: {
     textAlign: "center",
   },
   toggleContainer: {
     backgroundColor: "transparent",
     margin: 64,
-  },
-  toggleContainerButton: {
-    alignItems: "center",
-    alignSelf: "flex-end",
   },
 });
