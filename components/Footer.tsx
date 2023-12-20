d9b73487fe43aea2b03388153a4839824db2a573
Simoncar <simoncar@gmail.com>
2023-12-20 13:40:57 +0000

    Add footer actions and toggle functionality

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   Dimensions,
@@ -20,6 +20,7 @@ type Props = {
 
 const Footer = ({ project, post }: Props) => {
   const colorScheme = useColorScheme();
+  const [footer, setFooter] = useState<boolean>(false);
 
   // create a functiont that accepts a timestamp and returns a string of the relative time from now
   const getRelativeTime = (timestamp: number) => {
@@ -37,15 +38,91 @@ const Footer = ({ project, post }: Props) => {
         <Text style={styles.leftText}>
           {post.author} · {getRelativeTime(post.timestamp?.toDate() ?? 0)}
         </Text>
-        <Text style={styles.rightText}>
-          <Ionicons name="ellipsis-horizontal" size={20} />
-        </Text>
+        <Pressable onPress={() => setFooter(!footer)}>
+          <Text style={styles.rightText}>
+            <Ionicons name="ellipsis-horizontal" size={20} />
+          </Text>
+        </Pressable>
       </View>
+      {footer && (
+        <View style={styles.boxContainer}>
+          <View style={styles.box}>
+            <View style={styles.actionRow}>
+              <Text style={styles.actionLeftText}>Share</Text>
+
+              <Ionicons
+                style={[
+                  styles.actionRightIcon,
+                  { color: Colors[colorScheme ?? "light"].text },
+                ]}
+                name="share-outline"
+                size={25}
+              />
+            </View>
+            <View style={styles.line} />
+            <View style={styles.actionRow}>
+              <Text style={styles.actionLeftText}>Edit</Text>
+
+              <Ionicons
+                style={[
+                  styles.actionRightIcon,
+                  { color: Colors[colorScheme ?? "light"].text },
+                ]}
+                name="ios-pencil"
+                size={25}
+              />
+            </View>
+            <View style={styles.line} />
+
+            <View style={styles.actionRow}>
+              <Text style={[styles.actionLeftText, { color: "red" }]}>
+                Delete
+              </Text>
+              <Ionicons
+                style={[styles.actionRightIcon, { color: "red" }]}
+                name="md-trash-outline"
+                size={25}
+              />
+            </View>
+          </View>
+        </View>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  actionLeftText: {
+    fontSize: 18,
+  },
+  actionRightIcon: {
+    textAlign: "right",
+  },
+  actionRow: {
+    alignItems: "center",
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    height: 40,
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    width: "100%",
+  },
+  box: {
+    backgroundColor: "transparent",
+    borderColor: "grey",
+    borderRadius: 10,
+    borderWidth: 1,
+    width: "66%",
+  },
+  boxContainer: {
+    alignItems: "center",
+    backgroundColor: "transparent",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginTop: 15,
+  },
   container: {
     backgroundColor: "transparent",
     paddingTop: 10,
@@ -53,6 +130,11 @@ const styles = StyleSheet.create({
   leftText: {
     color: "grey",
     fontSize: 12,
+  },
+  line: {
+    backgroundColor: "grey",
+    height: 1,
+    width: "100%",
   },
   rightText: {
     color: "grey",
