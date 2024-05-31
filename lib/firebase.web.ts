b026959cd1230b928cc03002222950a2db3aaf85
Simoncar <simoncar@gmail.com>
2024-05-31 18:41:04 +0200

    chore: Update firebase dependencies to latest versions
    - Update @react-native-firebase/app to version 20.0.0
    - Update @react-native-firebase/auth to version 20.0.0
    - Update @react-native-firebase/firestore to version 20.0.0
    - Update @react-native-firebase/storage to version 20.0.0
    - Update firebase to version 10.12.2

--- lib/firebase.web.ts
+++ lib/firebase.web.ts
@@ -25,46 +25,8 @@ const firebaseConfig = {
 
 const app = firebase.initializeApp(firebaseConfig);
 
-//
-
-// export { firebase, db, storage, auth, firestore };
 const auth = firebase.auth;
 const db = firebase.firestore();
-//const storage = firebase.storage();
 const firestore = firebase.firestore;
 
-//db.useEmulator("127.0.0.1", 8080);
-//auth().useEmulator("http://localhost:9099");
-//storage().useEmulator("127.0.0.1", 9199);
-//
 export { firebase, db, storage, auth, firestore, uploadBytes };
-
-// export const firebaseErrors: Record<string, string> = {
-//   "Firebase: Error (auth/email-already-in-use).":
-//     "This email is already is use.",
-//   "Firebase: Error (auth/popup-closed-by-user).":
-//     "Process got canceled by user.",
-//   "Firebase: Error (auth/user-not-found).": "User not found.",
-//   "Firebase: Error (auth/wrong-password).": "Wrong email or password.",
-//   "Firebase: Error (auth/network-request-failed).":
-//     "Failed to send the request. Check your connection.",
-// };
-
-// const db = initializeApp(firebaseConfig);
-
-// // const db = firestore(app);
-
-// // firestore()
-// //   .settings({
-// //     persistence: true,
-// //   })
-// //   .then(() => {
-// //     if (!Device.isDevice) {
-// //       console.log("Connecting to Firebase Emulator");
-// //       db.useEmulator("127.0.0.1", 8080);
-// //       auth().useEmulator("http://localhost:9099");
-// //       storage().useEmulator("127.0.0.1", 9199);
-// //     }
-// //   });
-
-// export { firebase, db, storage, auth, firestore };
