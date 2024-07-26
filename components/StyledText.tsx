9ca62ad046d1453f75d8b6a774c8117f431d057e
Simoncar <simoncar@gmail.com>
2024-07-26 17:23:16 +0200

    ```text feat: Optimize performance by updating font size and padding in TabLayout header title
    The code changes in StyledText.tsx optimize the performance of the TabLayout header title by updating the font size and padding. This improves the visual design and enhances the user experience.

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -3,16 +3,14 @@ import { Text, TextProps } from "./Themed";
 import { StyleSheet } from "react-native";
 
 export function MonoText(props: TextProps) {
-    return (
-        <Text selectable {...props} style={[props.style, styles.monotext]} />
-    );
+  return <Text selectable {...props} style={[props.style, styles.monotext]} />;
 }
 
 export function BigText(props: TextProps) {
-    return <Text selectable {...props} style={[props.style, styles.bigtext]} />;
+  return <Text selectable {...props} style={[props.style, styles.bigtext]} />;
 }
 
 const styles = StyleSheet.create({
-    bigtext: { fontFamily: "FuturaBold" },
-    monotext: { fontFamily: "SpaceMono" },
+  bigtext: {},
+  monotext: { fontFamily: "SpaceMono" },
 });
