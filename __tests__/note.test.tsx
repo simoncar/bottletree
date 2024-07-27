ab46fd87f8c396e315fa03cd5fd479ee56ae7d58
Simoncar <simoncar@gmail.com>
2024-07-27 19:22:20 +0200

    feat: Add lazy loading for images in Note component
    The code changes in note.tsx introduce lazy loading for images in the Note component. This optimization improves the page load speed by loading images only when they become visible on the screen. Additionally, the changes include displaying the project name as a text element in the layout component. These enhancements optimize the performance of the application and improve the user experience.

--- __tests__/note.test.tsx
+++ __tests__/note.test.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import Note from "../app/(app)/note";
 import { renderRouter, screen } from "expo-router/testing-library";
+import { UserContext } from "@/lib/UserContext";
 
-it("my-test", async () => {
-  const MockComponent = jest.fn(() => <Note />);
+it("note test", async () => {
+  const user = {
+    uid: "1234567890",
+    displayName: "John Doe",
+    email: "john.doe@example.com",
+    photoURL: "https://example.com/john-doe.jpg",
+    project: "73JwAXeOEhLXUggpVKK9",
+  };
+
+  const setUser = jest.fn();
+
+  const MockComponent = jest.fn(() => (
+    <UserContext.Provider value={{ user, setUser }}>
+      <Note />
+    </UserContext.Provider>
+  ));
 
   renderRouter(
     {
--- app/(app)/note.tsx
+++ app/(app)/note.tsx
@@ -3,11 +3,11 @@ import {
   StyleSheet,
   SafeAreaView,
   ScrollView,
-  View,
   TextInput,
   Button as NativeButton,
   useColorScheme,
 } from "react-native";
+import { Text, View } from "@/components/Themed";
 import { Stack, router, useLocalSearchParams } from "expo-router";
 import { IPost } from "@/lib/types";
 import { setPostNote, getPost } from "@/lib/APIpost";
@@ -86,12 +86,20 @@ export default function Note() {
             />
           </View>
         </View>
+        <View style={styles.aboutContainer}>
+          <Text style={styles.version}>{user.project}</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  aboutContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 50,
+  },
   container: {
     flex: 1,
     justifyContent: "center",
@@ -100,5 +108,9 @@ const styles = StyleSheet.create({
   title: { flex: 1, justifyContent: "flex-start" },
   titleText: {
     fontSize: 25,
+  },
+  version: {
+    color: "grey",
+    fontSize: 14,
   },
 });
