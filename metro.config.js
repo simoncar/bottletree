7979bc41a68e5a0328d6d6c6383d80910d54c8ce
Simoncar <simoncar@gmail.com>
2024-11-17 13:13:36 +0000

    Add react-native-reanimated plugin to Babel config and update dependencies

---
+++ metro.config.js
@@ -0,0 +1,10 @@
+const { getDefaultConfig } = require("expo/metro-config");
+
+const {
+	wrapWithReanimatedMetroConfig,
+} = require("react-native-reanimated/metro-config");
+
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = wrapWithReanimatedMetroConfig(config);
