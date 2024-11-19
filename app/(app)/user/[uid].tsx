8e4998fd7eb4fe0e8c1c5bc51393c35e1d89f492
Simoncar <simoncar@gmail.com>
2024-11-19 12:27:32 +0000

    Refactor editUser component to improve code clarity by removing unnecessary comments and simplifying return statements

--- app/(app)/user/[uid].tsx
+++ app/(app)/user/[uid].tsx
@@ -55,7 +55,7 @@ export default function editUser() {
         ratio = myArray[0];
       }
 
-      return myArray[1]; // For example, creating a new array with each element doubled.
+      return myArray[1];
     });
 
     setUser({ ...user, photoURL: downloadURLarray[0] });
@@ -113,7 +113,6 @@ export default function editUser() {
   };
 
   const profilePic = () => {
-    //if user is null return nothing
     if (!user) {
       return;
     }
