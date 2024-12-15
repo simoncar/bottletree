7a4fe37068cd435754209ff1f41433eb3087556a
Simoncar <simoncar@gmail.com>
2024-12-15 11:03:45 +0000

    Update app configuration to support environment-specific settings and improve splash screen background color

--- app.config.js
+++ app.config.js
@@ -1,79 +1,83 @@
-module.exports = {
-	expo: {
-		name: "One Build",
-		slug: "bottletree",
-		version: "1.0.190",
-		orientation: "portrait",
-		icon: "./assets/images/icon.png",
-		scheme: "bottletree",
-		userInterfaceStyle: "automatic",
-		newArchEnabled: true,
-		splash: {
-			image: "./assets/images/splash.png",
-			resizeMode: "contain",
-			backgroundColor: "#5D5CE7"
-		},
-		assetBundlePatterns: ["**/*"],
-		ios: {
-			googleServicesFile: "./GoogleService-Info.plist",
-			supportsTablet: true,
-			bundleIdentifier: "com.simoncar.bottletree",
-			config: {
-				usesNonExemptEncryption: false
+module.exports = ({ config }) => {
+	const isDev = process.env.APP_ENV === "development";
+
+	return {
+		expo: {
+			name: isDev ? "One Build DEV" : "One Build",
+			slug: "bottletree",
+			version: "1.0.190",
+			orientation: "portrait",
+			icon: "./assets/images/icon.png",
+			scheme: "bottletree",
+			userInterfaceStyle: "automatic",
+			newArchEnabled: true,
+			splash: {
+				image: "./assets/images/splash.png",
+				resizeMode: "contain",
+				backgroundColor: isDev ? "#5D5CE7" : "#F9D96D",
 			},
-			associatedDomains: ["applinks:b.otbapps.com", "applinks:b.simon.co"]
-		},
-		android: {
-			googleServicesFile: "./google-services.json",
-			adaptiveIcon: {
-				foregroundImage: "./assets/images/adaptive-icon.png",
-				backgroundColor: "#5D5CE7"
+			assetBundlePatterns: ["**/*"],
+			ios: {
+				googleServicesFile: "./GoogleService-Info.plist",
+				supportsTablet: true,
+				bundleIdentifier: "com.simoncar.bottletree",
+				config: {
+					usesNonExemptEncryption: false
+				},
+				associatedDomains: ["applinks:b.otbapps.com", "applinks:b.simon.co"]
 			},
-			package: process.env.ANDROID_PACKAGE || "co.simon.bottletree"
-		},
-		web: {
-			bundler: "metro",
-			output: "static",
-			favicon: "./assets/images/favicon.png"
-		},
-		extra: {
-			router: {
-				origin: false
+			android: {
+				googleServicesFile: "./google-services.json",
+				adaptiveIcon: {
+					foregroundImage: "./assets/images/adaptive-icon.png",
+					backgroundColor: isDev ? "#5D5CE7" : "#F9D96D",
+				},
+				package: process.env.ANDROID_PACKAGE || "co.simon.bottletree"
 			},
-			eas: {
-				projectId: "c4340333-f23a-41cb-a647-97dee3bc0a01"
-			}
-		},
-		runtimeVersion: {
-			policy: "sdkVersion"
-		},
-		updates: {
-			url: "https://u.expo.dev/c4340333-f23a-41cb-a647-97dee3bc0a01"
-		},
-		experiments: {
-			typedRoutes: true
-		},
-		plugins: [
-			"@react-native-firebase/app",
-			"@react-native-firebase/auth",
-			[
-				"expo-image-picker",
-				{
-					photosPermission: "Allow $(PRODUCT_NAME) to access your photos to share the images you select with other people that have access to the project."
+			web: {
+				bundler: "metro",
+				output: "static",
+				favicon: "./assets/images/favicon.png"
+			},
+			extra: {
+				router: {
+					origin: false
+				},
+				eas: {
+					projectId: "c4340333-f23a-41cb-a647-97dee3bc0a01"
 				}
-			],
-			[
-				"expo-contacts",
-				{
-					contactsPermission: "Allow $(PRODUCT_NAME) to access your contacts to select people you want to share a project with."
-				}
-			],
-			[
-				"expo-camera",
-				{
-					cameraPermission: "Allow $(PRODUCT_NAME) to access your camera to take photos and videos."
-				}
+			},
+			runtimeVersion: {
+				policy: "sdkVersion"
+			},
+			updates: {
+				url: "https://u.expo.dev/c4340333-f23a-41cb-a647-97dee3bc0a01"
+			},
+			experiments: {
+				typedRoutes: true
+			},
+			plugins: [
+				"@react-native-firebase/app",
+				"@react-native-firebase/auth",
+				[
+					"expo-image-picker",
+					{
+						photosPermission: "Allow $(PRODUCT_NAME) to access your photos to share the images you select with other people that have access to the project."
+					}
+				],
+				[
+					"expo-contacts",
+					{
+						contactsPermission: "Allow $(PRODUCT_NAME) to access your contacts to select people you want to share a project with."
+					}
+				],
+				[
+					"expo-camera",
+					{
+						cameraPermission: "Allow $(PRODUCT_NAME) to access your camera to take photos and videos."
+					}
+				]
 			]
-		]
+		}
 	}
-};
+}
