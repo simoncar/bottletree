37ed33018f01adf379fe2f93413dc979b0344047
Simoncar <simoncar@gmail.com>
2024-12-19 16:35:17 +0000

    Bump app version to 1.0.191

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ module.exports = ({ config }) => {
 		expo: {
 			name: isDev ? "One Build DEV" : "One Build",
 			slug: "bottletree",
-			version: "1.0.190",
+			version: "1.0.191",
 			orientation: "portrait",
 			icon: "./assets/images/icon.png",
 			scheme: "bottletree",
