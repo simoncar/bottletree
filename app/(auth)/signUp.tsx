fb67d024aa853cac6a9c8d0ceb0317009ba0d66b
Simoncar <simoncar@gmail.com>
2025-01-09 14:12:38 +0000

    Improve error handling and update success message in SignIn component

--- app/(auth)/signUp.tsx
+++ app/(auth)/signUp.tsx
@@ -42,13 +42,25 @@ export default function SignIn() {
       router.navigate("/");
     } else {
       //updateSharedDataUser(null);
+      if (error && typeof error.message === "string") {
+        setErrorMessage(strip(error.message));
+      } else {
+        setErrorMessage("Unknown Error");
+      }
     }
-    setErrorMessage(strip(error));
   };
 
   const strip = (str: string) => {
     if (!str) return "";
-    return str.substring(str.lastIndexOf("]") + 1).trim();
+    let returnMessage = "";
+    try {
+      returnMessage = str.substring(str.lastIndexOf("]") + 1).trim();
+    } catch (error) {
+      console.error("Error stripping string:", error);
+      returnMessage = str;
+    }
+
+    return returnMessage;
   };
 
   const renderOldUser = () => {
@@ -64,7 +76,7 @@ export default function SignIn() {
   };
 
   const renderAction = (errorMessage: string) => {
-    if (errorMessage == "Success") {
+    if (errorMessage == "SignUp Success") {
       return;
     } else {
       return (
@@ -83,52 +95,52 @@ export default function SignIn() {
     <View style={styles.container}>
       <Stack.Screen options={{ title: "Create Account" }} />
 
-	<View style={styles.inputView}>
-	  <TextInput
-		style={styles.textInput}
-		inputMode="text"
-		placeholder="Name"
-		autoCorrect={false}
-		autoFocus
-		autoComplete="name"
-		textContentType="name"
-		onChangeText={(name) => setName(name)}
-	  />
-	</View>
-	<View style={styles.inputView}>
-	  <TextInput
-		style={styles.textInput}
-		keyboardType="email-address"
-		inputMode="email"
-		autoCapitalize="none"
-		placeholder="Email"
-		autoComplete="email"
-		spellCheck={false}
-		textContentType="emailAddress"
-		onChangeText={(email) => setEmail(email)}
-	  />
-	</View>
-	<View style={styles.inputView}>
-	  <TextInput
-		style={styles.textInput}
-		placeholder="Password"
-		secureTextEntry={secureEntry}
-		autoComplete="password"
-		textContentType="password"
-		onChangeText={(password) => {
-		setPassword(password);
-		setErrorMessage("");
-		}}
-	  />
-	  <Pressable onPress={() => setSecureEntry(!secureEntry)}>
-		<AntDesign
-		name="eye"
-		size={25}
-		style={styles.eye}
-		color={Colors[colorScheme ?? "light"].text}
-		/>
-	  </Pressable>
-	</View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.textInput}
+          inputMode="text"
+          placeholder="Name"
+          autoCorrect={false}
+          autoFocus
+          autoComplete="name"
+          textContentType="name"
+          onChangeText={(name) => setName(name)}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.textInput}
+          keyboardType="email-address"
+          inputMode="email"
+          autoCapitalize="none"
+          placeholder="Email"
+          autoComplete="email"
+          spellCheck={false}
+          textContentType="emailAddress"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Password"
+          secureTextEntry={secureEntry}
+          autoComplete="password"
+          textContentType="password"
+          onChangeText={(password) => {
+            setPassword(password);
+            setErrorMessage("");
+          }}
+        />
+        <Pressable onPress={() => setSecureEntry(!secureEntry)}>
+          <AntDesign
+            name="eye"
+            size={25}
+            style={styles.eye}
+            color={Colors[colorScheme ?? "light"].text}
+          />
+        </Pressable>
+      </View>
       <View style={styles.notificationView}>
         <Text numberOfLines={3} style={styles.notificationText}>
           {errorMessage}
