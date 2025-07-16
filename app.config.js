ab4b7ddd678128876003a4df8b0e5cc843cd606a
Simoncar <simoncar@gmail.com>
2025-07-16 17:43:23 +0100

    Update: increment app version from 1.0.300 to 1.0.303

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ module.exports = ({ config }) => {
 		expo: {
 			name: isDev ? "One Build DEV" : "One Build",
 			slug: "bottletree",
-			version: "1.0.300",
+			version: "1.0.303",
 			orientation: "default",
 			icon: "./assets/images/icon.png",
 			scheme: "bottletree",
