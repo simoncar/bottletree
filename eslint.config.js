b2d5a2f0d21fc5cab1c289696d496784be907b7d
Simoncar <simoncar@gmail.com>
2025-07-29 15:49:55 +0200

    Refactor: remove unused ESLint and Prettier configurations, update package.json scripts for linting and formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,10 @@
-// eslint.config.js
-const { defineConfig } = require("eslint/config");
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
-	{
-		plugins: {
-			"@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
-			"react": require("eslint-plugin-react"),
-			"react-native": require("eslint-plugin-react-native"),
-
-		},
-		rules: {
-			semi: "error",
-			"prefer-const": "error",
-			"linebreak-style": ["error", "unix"],
-			"quotes": ["error", "double"],
-			"react-native/no-unused-styles": 2,
-			"react-native/split-platform-components": 2,
-			"@typescript-eslint/no-explicit-any": "off",
-			"react-native/no-inline-styles": 2,
-			"react-native/no-color-literals": 0,
-			"react-native/no-raw-text": 2,
-			"react-native/no-single-element-style-arrays": 2,
-			"@typescript-eslint/explicit-function-return-type": "warn",
-			"@typescript-eslint/no-unused-vars": "off",
-			// Import sorting
-			'import/order': ['error', {
-			'groups': ['builtin', 'external', 'internal'],
-			'pathGroups': [
-				{ pattern: 'react', group: 'external', position: 'before' },
-				{ pattern: 'expo**', group: 'external', position: 'before' },
-			],
-			'pathGroupsExcludedImportTypes': ['react'],
-			'newlines-between': 'always',
-			'alphabetize': { order: 'asc', caseInsensitive: true },
-			}],
-		},
-	}
+  expoConfig,
+  {
+    ignores: ["dist/*"],
+  }
 ]);
