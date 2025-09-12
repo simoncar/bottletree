dd70e27984df2d65578c08b602a2afb6963e3448
Simoncar <simoncar@gmail.com>
2025-09-12 18:03:57 +0200

    refactor: remove babel configuration and update metro.config.js for cleaner setup

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,5 @@
-//const { getDefaultConfig } = require("expo/metro-config");
-const { getSentryExpoConfig } = require('@sentry/react-native/metro');
-const {
-	wrapWithReanimatedMetroConfig,
-} = require("react-native-reanimated/metro-config");
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
-
-//const config = getDefaultConfig(__dirname);
 const config = getSentryExpoConfig(__dirname);
 
-module.exports = wrapWithReanimatedMetroConfig(config);
+module.exports = config;
