8bfbdc42f0681a21896bfd1ee4e8905c1f546235
Simoncar <simoncar@gmail.com>
2025-09-15 16:58:48 +0200

    fix: remove hardcoded Firebase configuration and use environment variables

--- lib/firebase.web.ts
+++ lib/firebase.web.ts
@@ -34,17 +34,6 @@ import { getStorage } from "firebase/storage";
 import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-u3kUGycFCQQC5S3r2p2nQAGPRhKiMpE",
-  authDomain: "builder-403d5.firebaseapp.com",
-  databaseURL: "https://builder-403d5-default-rtdb.firebaseio.com",
-  projectId: "builder-403d5",
-  storageBucket: "builder-403d5.appspot.com",
-  messagingSenderId: "813298396785",
-  appId: "1:813298396785:web:80b06b25db284086d175d0",
-  measurementId: "G-37RWRJQHKX",
-};
-
-const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
