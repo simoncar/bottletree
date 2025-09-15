4e92d86259bd11f0ed98fe7392e7cbc047f6d8c8
Simoncar <simoncar@gmail.com>
2025-09-15 17:17:55 +0200

    fix: update Firebase configuration to use EXPO environment variables

--- lib/firebase.web.ts
+++ lib/firebase.web.ts
@@ -33,16 +33,23 @@ import {
 import { getStorage } from "firebase/storage";
 import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
 
+console.log(
+  "Using web version of Firebase:",
+  process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+);
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
 };
+
+console.log("firebaseConfig:", firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const dbm = getFirestore(app);
