05dbe4d61a670610a4adc2dd1772a0bca4fa5763
Simoncar <simoncar@gmail.com>
2025-09-15 16:51:08 +0200

    fix: update firebase configuration to use environment variables for sensitive data

--- lib/firebase.web.ts
+++ lib/firebase.web.ts
@@ -44,6 +44,17 @@ const firebaseConfig = {
   measurementId: "G-37RWRJQHKX",
 };
 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+};
+
 const app = initializeApp(firebaseConfig);
 const dbm = getFirestore(app);
 const auth = getAuth(app);
