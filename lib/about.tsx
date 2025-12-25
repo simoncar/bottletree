5dcbb60dbcc0f95a4c6e78399ac397ad95d13348
Simoncar <simoncar@gmail.com>
2025-12-25 09:52:47 +0100

    fix: update application version in About component

--- lib/about.tsx
+++ lib/about.tsx
@@ -37,7 +37,7 @@ export const About = () => {
         </Text>
         <Text style={styles.version}>
           {Application.nativeApplicationVersion} (
-          {Application.nativeBuildVersion}) | 888.a.app
+          {Application.nativeBuildVersion}) | 888.b.25.app
         </Text>
         <Text style={styles.version}>User Context - {user.uid}</Text>
         <Text style={styles.version}>CTX - {session}</Text>
